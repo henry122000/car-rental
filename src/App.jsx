8d24e3f4a887d01dc3adde1314c5d87c4cf3592d
henry122000 <mhoang0102@gmail.com>
2024-11-29 02:59:13 -0800

    add service page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Services from "./components/Services/Services";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
@@ -33,12 +34,13 @@ const App = () => {
     });
     AOS.refresh();
   }, []);
-  
+
   return (
     <div>
       <Navbar theme={theme} setTheme={setTheme} />
       <Home />
       <About />
+      <Services />
     </div>
   )
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,26 +3,31 @@ import carPng from '../../assets/aboutCar.png'
 
 const About = () => {
   return (
-    <div className='dark:bg-dark dark:text-white 
+    <div className='dark:bg-dark bg-slate-100 dark:text-white 
     duration-300 sm:min-h-[600px] 
     sm:grid sm:place-items-center'>
       <div className="container">
         <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
-            <div>
+            <div
+            data-aos="slide-right"
+            data-aos-duration="1500"
+            >
             <img src={carPng} alt='Car in About Page' 
             className='sm:scale-105 sm:-translate-x-11 max-h-[300px]'/>
             </div>
             <div>
                 <div className='space-y-5 sm:p-16 pb-6'>
-                    <h1 className='text-3xl sm:text-4xl font-bold font-serif'>About us</h1>
-                    <p>At FlexiRide, we believe that every journey should be as exciting as the destination. 
+                    <h1 className='text-3xl sm:text-4xl font-bold font-serif'
+                    data-aos="fade-down">About us</h1>
+                    <p data-aos="fade-down">At FlexiRide, we believe that every journey should be as exciting as the destination. 
                         Founded with a passion for seamless travel and a commitment to exceptional customer service, 
                         we offer a wide range of vehicles tailored to your needs. From compact city cars to luxurious SUVs, 
                         our fleet is designed to deliver comfort, safety, and style—every mile of the way.</p>
-                    <p>With transparent pricing, flexible rental options, and 24/7 support, we make car rental simple, 
+                    <p data-aos="fade-down">With transparent pricing, flexible rental options, and 24/7 support, we make car rental simple, 
                         affordable, and stress-free. Whether you're embarking on a business trip, family vacation, 
                         or spontaneous adventure, we’re here to help you find the perfect ride and create 
                         unforgettable memories on the road.</p>
+                    <button data-aos="fade-down" className='button-outline'>Book Now</button>
                 </div>
             </div>
         </div>
